1a17b34c1f8deae18c23749a216cedf2f61b6ad1
Paulo Carabalone <psoreto@outlook.com>
2022-02-01 08:36:32 -0300

    rgss font and cache placeholder

---
+++ client/Scripts/rgss/cache.rb
@@ -0,0 +1,3 @@
+module Cache
+  extend self
+end
---
+++ client/Scripts/rgss/font.rb
@@ -0,0 +1,55 @@
+class Font
+  def initialize(name, size)
+  end
+
+  def self.exist?(name)
+  end
+
+  def name
+  end
+
+  def name=(v)
+  end
+
+  def size
+  end
+
+  def size=(v)
+  end
+
+  def bold
+  end
+
+  def bold=(v)
+  end
+
+  def italic
+  end
+
+  def italic=(v)
+  end
+
+  def outline
+  end
+
+  def outline=(v)
+  end
+
+  def shadow
+  end
+
+  def shadow=(v)
+  end
+
+  def color
+  end
+
+  def color=(v)
+  end
+
+  def out_color
+  end
+
+  def out_color=(v)
+  end
+end
