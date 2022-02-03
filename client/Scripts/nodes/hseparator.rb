72e0b9986d99f3b36c112b47b78a75a39addf4a2
Paulo Carabalone <psoreto@outlook.com>
2022-02-03 17:11:59 -0300

    Separator, HSeparator and VSeparator added

---
+++ client/Scripts/nodes/hseparator.rb
@@ -0,0 +1,6 @@
+class HSeparator < Separator
+  def initialize(source = nil)
+    init_source source
+    init_inner_control_vars
+  end
+end
---
+++ client/Scripts/nodes/separator.rb
@@ -0,0 +1,5 @@
+class Separator < Control
+  def initialize
+    fail_to_init
+  end
+end
