5801b992c3b4663529848e974bda62d7d9886b13
Valentine <marlosgama@outlook.com>
2022-03-29 20:08:31 -0300

    Update node2d.rb

--- client/Scripts/nodes/node2d.rb
+++ client/Scripts/nodes/node2d.rb
@@ -7,4 +7,18 @@ class Node2D < CanvasItem
   def current_godot_object
     "Node2D"
   end
+  
+  def init_inner_control_vars
+    super
+    @position = Vector2.new @source.position
+  end
+
+  def position
+    @position
+  end
+
+  def position=(v)
+    @position = v
+    @source.position = v.source
+  end
 end
